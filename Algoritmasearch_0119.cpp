d854e29268d20a763be9928c132cf2eb0e2fd095
kiieeaa <kiyaadinda5@gmail.com>
2024-04-05 10:08:23 +0800

    accept array elements

--- Algoritmasearch_0119.cpp
+++ Algoritmasearch_0119.cpp
@@ -9,9 +9,14 @@ void input()
 {
 	while (true)
 	{
-
 		cout << " enter the number of elemen in the array: ";
 		cin >> n;
 		if ((n > 0) && (n <= 20))
 			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+
+
+	cout << "\n----------\n";
 
